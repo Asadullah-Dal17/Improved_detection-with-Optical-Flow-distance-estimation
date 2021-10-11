35c4ce936031f696bfd2282ee30ea198e2f9c613
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-10-11 19:24:39 +0500

    point selection

---
+++ select_point_with_mouse/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2 as cv 
+import AiPhile
+import time
+def selectPoint(event, x , y , flags, parmas):
+    global point, condition
+    if event ==cv.EVENT_LBUTTONUP:
+        point =(int(x), int(y))
+        
+        condition =True 
+
+cv.namedWindow('frame')
+cv.setMouseCallback('frame', selectPoint)
+
+cap = cv.VideoCapture(0)
+
+frame_counter=0
+point =()
+condition = False
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame_counter += 1
+    fps = frame_counter/(time.perf_counter())
+    AiPhile.textBGoutline(frame, f'FPS: {round(fps,2)}', (40,30), scaling=0.8)
+    if condition:
+        cv.circle(frame, point, 5, AiPhile.MAGENTA, -1, cv.LINE_AA)
+
+    cv.imshow('frame', frame)
+    key = cv.waitKey(1)
+    if key ==ord('q'):
+        break
+cv.destroyAllWindows()
+cap.release()
